37f639c24cdf269436c33a6e0a0520d91077e7b6
awiedenhoeft <wiedenha@oregonstate.edu>
2025-02-27 22:48:44 -0800

    added update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /* SETUP */
 var express = require('express');
 var app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 PORT = 1996;
 
-
-// app.js
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
@@ -15,10 +16,6 @@ app.set('view engine', '.hbs');                 // Tell express to use the handl
 var db = require('./database/db-connector');
 
 /* ROUTES */
-// app.js 
-
-// app.js
-
 app.get('/', function(req, res)
     {  
         let query1 = "SELECT * FROM weddings;";               // Define our query
@@ -27,7 +24,37 @@ app.get('/', function(req, res)
 
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });                                                         // received back from the query
+    });        
+
+// app.js
+
+app.post('/add-wedding-form', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO weddings (clientID, weddingDate, location, weddingType, totalBudget) VALUES ('${data['input-clientID']}', '${data['input-weddingDate']}', '${data['input-location']}', '${data['input-weddingType']}', '${data['input-totalBudget']}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+});
 
 
 /* LISTENER */
